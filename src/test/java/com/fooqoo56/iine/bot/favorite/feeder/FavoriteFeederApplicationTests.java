63ff640d0e29df79a32132d113a120eadbebf60f
FooQoo <horseshoes.buzz.17@gmail.com>
2021-04-17 23:27:25 +0900

    add project

---
+++ src/test/java/com/fooqoo56/iine/bot/favorite/feeder/FavoriteFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooqoo56.iine.bot.favorite.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteFeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
